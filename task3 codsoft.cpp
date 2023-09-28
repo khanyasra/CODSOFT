3b9d8e615860bca06554f20345dc5b54e890069f
khanyasra <144794044+khanyasra@users.noreply.github.com>
2023-09-28 18:07:05 +0500

    Add files via upload

--- task3 codsoft.cpp
+++ task3 codsoft.cpp
@@ -5,7 +5,7 @@ using namespace std;
 char board[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
 int choice;
 int row;
-int coloum;
+int column;
 char turn = 'x';
 bool tie = false;
 void gameboard()
@@ -20,6 +20,22 @@ void gameboard()
 	cout <<"\t\t" << board[2][0] << "  |" << board[2][1] << "  |" << board[2][2] << endl;
 	cout << "\t\t   |   |   " << endl;
 }
+//void restart(){
+//	int a;
+//	for(row=0;row<3;row++){
+//		for(column=0;column<3;column++){
+//			*(*(board + row) + column)=NULL;
+//		}
+//	}
+//	delete[] board;
+//	for(row=0;row<3;row++){
+//		for(column=0;column<3;column++){
+//			*(*(board + row) + column)=a;
+//			a++;
+//		}
+//	}
+//	gameboard();
+//}
 void turn_function(){
 	if (turn == 'x') {
 		cout << "\nPlayer-1 [x]:\nchoice:- ";
@@ -32,50 +48,50 @@ void turn_function(){
 	switch (choice) {
 	case 1:
 		row = 0; 
-		coloum = 0;
+		column = 0;
 		break;
 	case 2:
 		row = 0;
-		coloum = 1;
+		column = 1;
 		break;
 	case 3:
 		row = 0;
-		coloum = 2;
+		column = 2;
 		break;
 	case 4:
 		row = 1;
-		coloum = 0;
+		column = 0;
 		break;
 	case 5:
 		row = 1;
-		coloum = 1;
+		column = 1;
 		break;
 	case 6:
 		row = 1;
-		coloum = 2;
+		column = 2;
 		break;
 	case 7:
 		row = 2;
-		coloum = 0;
+		column = 0;
 		break;
 	case 8:
 		row = 2;
-		coloum = 1;
+		column = 1;
 		break;
 	case 9:
 		row = 2;
-		coloum = 2;
+		column = 2;
 		break;
 	default:
 		cout << "\nInvalid choice!!!\n";
 	}
 
-	if (turn == 'x' && board[row][coloum] != 'x' && board[row][coloum] != 'o') {
-		board[row][coloum] = 'x';
+	if (turn == 'x' && board[row][column] != 'x' && board[row][column] != 'o') {
+		board[row][column] = 'x';
 		turn = 'o';
 	}
-	else if (turn == 'o' && board[row][coloum] != 'x' && board[row][coloum] != 'o') {
-		board[row][coloum] = 'o';
+	else if (turn == 'o' && board[row][column] != 'x' && board[row][column] != 'o') {
+		board[row][column] = 'o';
 		turn = 'x';
 	}
 	else {
@@ -107,19 +123,40 @@ bool result() {
 }
 
 int main() {
-	while (result()) {
-		cout << "\t\t\t\t\tWELCOME TO THE TICK--TAC--TOE GAME!!!\t\t\t\t\t\nPlayer - 1:-'X'\nPlayer - 2:-'O'\n ";
-		gameboard();
-		turn_function();
-		result();
-		system("cls");
-	}
-	if (turn == 'x' && tie == false) {
-		cout << "\n\nCongratulations!Player - 2 with 'O' has won the game" << endl<<endl;
-	}
-	else if (turn == 'o' && tie == false) {
-		cout << "\n\nCongratulations!Player - 1 with 'X' has won the game"<<endl<<endl;
-	}
-	else
-		cout << "\n\nGAME DRAW!!!" << endl <<endl;
+	char choice;
+	do{
+		cout<<"\nDo you want to play ??\n1.Yes(y)\n2.No(n)\nChoice = ";
+		cin>>choice;
+		cout<<endl;
+		if(choice=='y'||choice=='Y'){
+			do {
+				cout << "\t\t\t\t\tWELCOME TO THE TICK--TAC--TOE GAME!!!\t\t\t\t\t\nPlayer - 1:-'X'\nPlayer - 2:-'O'\n ";
+				gameboard();
+				turn_function();
+				result();
+				system("cls");
+			}while (result());
+			if (turn == 'x' && tie == false) {
+				cout << "\n\nCongratulations!Player - 2 with 'O' has won the game" << endl<<endl;
+			}
+			else if (turn == 'o' && tie == false) {
+				cout << "\n\nCongratulations!Player - 1 with 'X' has won the game"<<endl<<endl;
+			}
+			else
+			{
+				cout << "\n\nGAME DRAW!!!" << endl <<endl;
+			}
+		}
+		else if(choice=='N'||choice=='n'){
+	    	cout<<"Game End!"<<endl;
+	    	return 0;
+	    }
+        else{
+            cout<<"Enter a valid choice!";
+        }
+        delete[] board;
+
+//        restart();
+//        tie=false;
+	}while(choice!='Y'||choice!='y');
 }
